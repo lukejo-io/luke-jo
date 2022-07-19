dd9a8111cf53a8754b483b373fa649553d412389
Luke Johnson <lukejohnson@Lukes-MacBook-Air.local>
2022-07-18 20:20:32 -0500

    customize

---
+++ pages/resume.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Resume: NextPage = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Luke Johnson</h1>
+      <div>test</div>
+    </main>
+  );
+};
+
+export default Resume;
